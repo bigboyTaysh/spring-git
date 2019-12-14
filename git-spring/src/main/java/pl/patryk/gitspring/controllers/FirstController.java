8a08bb197f9154b90ee97b3a302b1d1075eb03e8
bigboyTaysh <pvtryk.w@gmail.com>
2019-12-14 13:02:19 +0100

    [change] - controller to restcontroller

--- git-spring/src/main/java/pl/patryk/gitspring/controllers/FirstController.java
+++ git-spring/src/main/java/pl/patryk/gitspring/controllers/FirstController.java
@@ -3,9 +3,10 @@ package pl.patryk.gitspring.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 import pl.patryk.gitspring.models.Person;
 
-@Controller
+@RestController
 public class FirstController {
 
     @GetMapping(value = "/users")
