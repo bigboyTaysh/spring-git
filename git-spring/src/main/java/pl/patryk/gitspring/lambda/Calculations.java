534e407201d19be0e345c098268884e927c19e7d
bigboyTaysh <pvtryk.w@gmail.com>
2019-12-15 09:13:44 +0100

    [change] - optional - change direction

--- git-spring/src/main/java/pl/patryk/gitspring/lambda/Calculations.java
+++
@@ -1,7 +0,0 @@
-package pl.patryk.gitspring.lambda;
-
-public interface Calculations {
-    int calc(int a, int b);
-
-    default void test(){};
-}
--- git-spring/src/main/java/pl/patryk/gitspring/lambda/StringInterface.java
+++
@@ -1,5 +0,0 @@
-package pl.patryk.gitspring.lambda;
-
-public interface StringInterface {
-    String strMethod(String imie, String nazwisko, String wiek);
-}
