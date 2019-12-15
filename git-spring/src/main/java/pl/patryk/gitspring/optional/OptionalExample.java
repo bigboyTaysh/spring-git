153a541c08e33dc7b815270ae9af3e9f1df76605
bigboyTaysh <pvtryk.w@gmail.com>
2019-12-15 08:07:08 +0100

    [change] - addd OptionalExample

---
+++ git-spring/src/main/java/pl/patryk/gitspring/optional/OptionalExample.java
@@ -0,0 +1,24 @@
+package pl.patryk.gitspring.optional;
+
+import java.util.Optional;
+
+public class OptionalExample {
+    public static void main(String[] args) {
+        Optional<String> maybeNick = findNick();
+
+        if (maybeNick.isPresent()) {
+            System.out.println(maybeNick.get());
+        }
+//nowy sposob lambda
+        maybeNick.ifPresent(
+                nick -> System.out.println("mam nick" + nick)
+        );
+
+    }
+
+    private static Optional<String> findNick() {
+        return Optional.empty();
+    }
+
+
+}
